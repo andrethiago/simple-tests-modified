34b48d34bfe1840768b40ee92135a44ce0837923
andre <andre@devel.devel>
2016-08-23 22:34:24 -0300

    Adição de comentários explicativos sobre o código.

--- src/test/java/br/mp/mpf/simpletests/testes/aceitacao/LoginSistemaSemPageObjectTest.java
+++ src/test/java/br/mp/mpf/simpletests/testes/aceitacao/LoginSistemaSemPageObjectTest.java
@@ -22,19 +22,18 @@ public class LoginSistemaSemPageObjectTest {
 
     @Test
     public void loginComSucesso() {
-
+	// Acessa a página inicial que nos redireciona para a página de login
 	driver.get("http://localhost:8080/simpletests");
 
+	// Informa o usuário/senha e clica no botão de login
 	driver.findElement(By.name("username")).sendKeys("joao@mpf.mp.br");
 	driver.findElement(By.name("password")).sendKeys("123456");
-
 	driver.findElement(By.cssSelector("input[type='submit'")).click();
 
-	// verifica se a barra de navegação existe
+	// Se a página inicial foi acessada: a barra de navegação existe e ela
+	// é a página de projetos
 	assertNotNull(driver.findElement(By.className("navbar")));
-	// verifica se a página inicial é a página de projetos
 	assertThat(driver.findElement(By.tagName("h2")).getText(), containsString("Projetos"));
-
     }
 
     @After
